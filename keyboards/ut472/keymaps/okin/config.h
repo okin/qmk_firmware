521e0775682803af5921e4173d8d7db9dec98d9b
Niko Wenselowski <der@nik0.de>
2020-11-29 00:37:47 +0100

    Make numpad comma / dot work better

--- keyboards/ut472/keymaps/okin/config.h
+++ keyboards/ut472/keymaps/okin/config.h
@@ -1,4 +1,4 @@
-/* Copyright 29 parlos Filoteo
+/* Copyright 2020 Niko Wenselowski
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
