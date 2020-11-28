bdabdeb58e2819f3e0f046f4336a1054876228db
Niko Wenselowski <der@nik0.de>
2020-11-29 00:40:16 +0100

    Make the top row numbers aswell on the numpad layer
    Makes it better for gaming

--- keyboards/ut472/keymaps/okin/keymap.c
+++ keyboards/ut472/keymaps/okin/keymap.c
@@ -139,7 +139,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * `-------------------------------------------------------------------------'
    */
   [_LAYER_NUM] = LAYOUT( /* Numbers */
-    _______, _______, _______, _______, _______, _______, XXXXXXX, KC_KP_7, KC_KP_8, KC_KP_9, XXXXXXX, _______,
+    _______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_KP_7, KC_KP_8, KC_KP_9, KC_0,    _______,
     _______, _______, _______, _______, _______, _______, XXXXXXX, KC_KP_4, KC_KP_5, KC_KP_6, XXXXXXX, _______,
     _______, _______, _______, _______, _______, _______, XXXXXXX, KC_KP_1, KC_KP_2, KC_KP_3, XXXXXXX, _______,
     _______, _______, _______, _______, _______,     _______,      KC_KP_0, NP_COMDOT, KC_NUMLOCK, TG(_LAYER_NUM), _______
